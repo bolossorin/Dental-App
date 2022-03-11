ad4deda7b6dc8d2b5315cd4bcabb8c4c86ab5bcc
Cloud <cloudcrowd1016@gmail.com>
2022-03-11 12:12:18 -0800

    added nextjs proxy

--- next.config.js
+++ next.config.js
@@ -3,5 +3,13 @@ module.exports = {
   env: {
     STRIPE_PK_KEY: process.env.STRIPE_PK_KEY,
     GOOGLE_KEY: process.env.GOOGLE_KEY,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://d2u0unnpqhu2n5.cloudfront.net/api/:path*' // Proxy to Backend
+      }
+    ]
   }
 };
