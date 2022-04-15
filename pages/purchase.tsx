11eb52d7907cc38cffe3dfa818356d91c4bc6a05
bolossorin <bolosvasilesorin@gmail.com>
2022-04-15 15:40:23 +0300

    fix: redirect to my-account

--- pages/purchase.tsx
+++ pages/purchase.tsx
@@ -1,22 +1,11 @@
-import React, {useContext, useEffect} from "react";
-
-//libs
-import Router from "next/router";
+import React from "react";
 
 // components
 import {Layout} from "../components";
 import {StripeCheckout} from "../components/stripe/StripeCheckout/StripeCheckout";
-import {AppContext} from "../context/app.context";
-import {routes} from "../utils/routes";
 
 const Payment = (): JSX.Element => {
-  const {state} = useContext(AppContext);
-  const {subscription_plan} = state.dentistState;
 
-  useEffect(() => {
-    const accessToken = localStorage.getItem('access_token')
-    if (subscription_plan === 'PREMIUM' || !accessToken) Router.push(routes.profile)
-  }, [subscription_plan])
   return (
     <Layout footer>
       <StripeCheckout setSubscriptionPlan={false} />
